e52fa8e2b8950164a8e45f896b61144275f2c36a
Yannick <yannick.vlassakoudis@gmail.com>
2021-07-24 22:12:35 +0200

    form object

--- frontend/floods/src/app/app-routing.module.ts
+++ frontend/floods/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ObjectFormComponent } from './components/object-form/object-form.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NewAccountComponent } from './pages/new-account/new-account.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path : 'nouveauCompte', component: NewAccountComponent },
   { path : 'offres', component: OffersComponent },
   { path : 'demandes', component: RequestsComponent },
+  { path : 'nouvelObjet', component: ObjectFormComponent },
 ];
 
 @NgModule({
